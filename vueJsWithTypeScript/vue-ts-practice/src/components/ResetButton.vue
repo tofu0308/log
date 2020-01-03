7d2945d46621c4c6b97147a1a292d8989df0a92f
tofu0308 <isisaka2000@gmail.com>
2020-01-03 13:43:11 +0900

    モデルバインド

---
+++ vueJsWithTypeScript/vue-ts-practice/src/components/ResetButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="onClick">Reset</button>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Emit, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class ResetButton extends Vue {
+        @Prop()
+        public initialValue!: string;
+
+        /** モデルバインドのために記述必須 */
+        @Prop()
+        public value!: string;
+
+        /** モデルバインドのために記述必須 */
+        @Emit()
+        public input(value: string) {}
+
+        public onClick() {
+            this.input(this.initialValue);
+        }
+    }
+</script>
--- vueJsWithTypeScript/vue-ts-practice/src/views/Home.vue
+++ vueJsWithTypeScript/vue-ts-practice/src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
     <div class="home">
         <p>{{greetText}}</p>
-        <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
+        <p>
+          <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
+        </p>
+        <p>
+          <ResetButton initialValue="Hello" v-model="greetText"></ResetButton>
+        </p>
     </div>
 </template>
 
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
     import MyButton from '@/components/MyButton.vue';
+    import ResetButton from '@/components/ResetButton.vue';
 
     @Component({
         components: {
             MyButton,
+            ResetButton,
       },
     })
     export default class Home extends Vue {
