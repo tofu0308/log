d66cb4b2532fa0d370d020d01ed8d591acd40fcf
tofu0308 <isisaka2000@gmail.com>
2020-01-07 23:12:53 +0900

    算出プロパティ

--- vueJsWithTypeScript/vue-ts-practice/src/views/Home.vue
+++ vueJsWithTypeScript/vue-ts-practice/src/views/Home.vue
@@ -2,6 +2,7 @@
     <div class="home">
         <p>{{greetText}}</p>
         <p>挨拶した回数：{{count}}回</p>
+        <p v-if="isRegulars">count >= 5 :isRegulars = true</p>
 
         <p>
           <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
@@ -26,6 +27,10 @@
     export default class Home extends Vue {
       public greetText: string = 'hello';
       private count = 0;
+
+      public get isRegulars(): boolean {
+        return this.count >= 5;
+      }
 
       public onMyButtonClicked(count: number) {
         this.count = count;
